ace1898062e24e792b50cdca2a07a0203fc6f00f
rremoissonnet <rudy.remoissonnet@orange.com>
2018-05-29 15:23:18 +0200

    base web application

---
+++ beer-web-app/src/main/java/com/orange/oab/beerwebapp/BeerWebApplication.java
@@ -0,0 +1,12 @@
+package com.orange.oab.beerwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerWebApplication.class, args);
+    }
+}
---
+++ beer-web-app/src/test/java/com/orange/oab/beerwebapp/BeerWebApplicationTest.java
@@ -0,0 +1,15 @@
+package com.orange.oab.beerwebapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BeerWebApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
